d4da91d9aeaf42cebc68d07be6c94f601e6d1ff5
Jakob Hauser <jakob.e.hauser@protonmail.com>
2023-02-15 22:59:45 +0100

    typo

--- shift_test.py
+++ shift_test.py
@@ -1,5 +1,7 @@
 from pi74HC595 import pi74HC595
-import RPI.GPIO as gpio
+import RPi.GPIO as gpio
 
 gpio.setmode(gpio.Board)
 shift_register = pi74HC595(DS =11, ST = 13, SH=15)
+
+shift_register.set_by_list([1, 1, 1, 1, 1, 1, 1, 1])
