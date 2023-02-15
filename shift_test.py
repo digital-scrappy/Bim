284d2ad05ec3edf556b71d6416e44bd08a86242e
Jakob Hauser <jakob.e.hauser@protonmail.com>
2023-02-15 23:07:34 +0100

    blink

--- shift_test.py
+++ shift_test.py
@@ -8,5 +8,5 @@ shift_register = pi74HC595(DS =11, ST = 13, SH=15)
 while True:
     shift_register.set_by_list([0, 0, 0, 0, 0, 0, 0, 0])
     sleep(2)
-    shift_register.set_by_list([1, 1, 1, 1, 1, 1, 1, 1])
+    shift_register.clear()
     sleep(2)
