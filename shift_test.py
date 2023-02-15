81c52a393b724c6429fb47cdbfef34d95574925b
Jakob Hauser <jakob.e.hauser@protonmail.com>
2023-02-15 23:06:15 +0100

    blink

--- shift_test.py
+++ shift_test.py
@@ -1,8 +1,12 @@
 from pi74HC595 import pi74HC595
 import RPi.GPIO as gpio
+from time import sleep
 
 gpio.setmode(gpio.BOARD)
 shift_register = pi74HC595(DS =11, ST = 13, SH=15)
 
 while True:
+    shift_register.set_by_list([0, 0, 0, 0, 0, 0, 0, 0])
+    sleep(2)
     shift_register.set_by_list([1, 1, 1, 1, 1, 1, 1, 1])
+    sleep(2)
